cf9f05bbc9b8385d70e5231452359d91d6c83970
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-15 18:49:48 +0530

    Solve q54 js

---
+++ q54-concat-two-strings-using-method/js/script.js
@@ -0,0 +1,9 @@
+function onClick() {
+	const str1 = "Hello, ";
+	const str2 = "World!";
+	const output = document.getElementById("output");
+
+	const newStr = str1.concat(str2);
+
+	output.textContent = "Concatenated string is: " + newStr;
+}
