e746f4a252a97a56caf7bc061ba03a3be4591e93
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-15 15:22:36 +0530

    Solve q19 js

---
+++ q19-day-in-a-week/js/script.js
@@ -0,0 +1,13 @@
+function onClick() {
+	const num = parseInt("4");
+	const output = document.getElementById("output");
+
+	const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+	if(num < 1 || num > 7) {
+		output.textContent = "Invalid day";
+	}
+	else {
+		const day = days[num-1];
+		output.textContent = "The equivalent day is " + day;
+	}
+}
