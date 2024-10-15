181e97b50597fb5fe813b5d0b56cd9bde55f1c06
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-15 14:25:44 +0530

    Solve qh16 js

---
+++ qh16-reverse-vowels-in-string/js/script.js
@@ -0,0 +1,24 @@
+function onClick() {
+	const str = "apuupile";
+	const output = document.getElementById("output");
+
+	const vowels = ['a','e','i','o','u','A','E','I','O','U'];
+	let arr = [];
+	let newStr = "";
+	for(let i=0; i<str.length; i++) {
+		if (vowels.includes(str[i])) {
+			arr.push(str[i]);
+		}
+	}
+
+	for(let i=0; i<str.length; i++) {
+		if (vowels.includes(str[i])) {
+			newStr += arr.pop();
+		}
+		else {
+			newStr += str[i];
+		}
+	}
+
+	output.textContent = "New String is " + newStr;
+}
