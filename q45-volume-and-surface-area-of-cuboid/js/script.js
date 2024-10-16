d353e936e7b557df8a4e4482f92af3549c0193ba
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-16 10:40:43 +0530

    Solve q45 js

---
+++ q45-volume-and-surface-area-of-cuboid/js/script.js
@@ -0,0 +1,14 @@
+function onClick() {
+	const length = parseFloat("12");
+	const width = parseFloat("4");
+	const height = parseFloat("13");
+	const output = document.getElementById("output");
+
+	const surfaceArea = 2 * (length * width + width * height + height * length);
+	const volume = length * width * height;
+	const lateralSurfaceArea = 2 * height * (length + width);
+	const result = `Total surface area: ${surfaceArea}\nVolume: ${volume}\nLateral surface area: ${lateralSurfaceArea}.`
+
+	output.setAttribute('style', 'white-space: pre;');
+	output.textContent = result;
+}
