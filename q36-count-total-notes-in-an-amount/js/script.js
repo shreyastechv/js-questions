92f9552074f68c1a288e5dd2972b0c2824a489e0
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-16 10:01:42 +0530

    Solve q36 js

---
+++ q36-count-total-notes-in-an-amount/js/script.js
@@ -0,0 +1,17 @@
+function onClick() {
+	const amount = parseInt("5880");
+	const output = document.getElementById("output");
+
+	let copyAmount = amount;
+	const notes = [2000, 500, 200, 100, 50, 20, 10];
+	let result = "";
+
+	for (let note of notes) {
+		console.log(note);
+		let count = Math.floor(copyAmount/note);
+		copyAmount = copyAmount % note;
+		result += `Count of ${note} notes is ${count}. `;
+	}
+
+	output.textContent = result;
+}
