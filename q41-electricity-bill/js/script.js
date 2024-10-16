6f2152968b555348e1dafeb9aae07cae467dc1b1
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-16 18:34:44 +0530

    Upload partially solved q41

---
+++ q41-electricity-bill/js/script.js
@@ -0,0 +1,12 @@
+function onClick() {
+	const units = parseInt(document.getElementById("units").value);
+	const output = document.getElementById("output");
+
+	let copyUnits = units;
+	let charge = 0;
+	if (copyUnits >= 50) {
+		
+	}
+
+	output.textContent = "";
+}
